115c5273cbb76d2886eca56b000daa9fc83b0f5b
Hubert Mazur <7mazur@taurus.fis.agh.edu.pl>
2019-04-25 14:30:32 +0200

    1

--- Lab_07/Zestaw_2/Main.cpp
+++ Lab_07/Zestaw_2/Main.cpp
@@ -7,59 +7,73 @@ int main ()
 {
 
 	int N = 7;
-	auto *function_values = new double[N];
-	auto *knots = new double[N];
+	double *function_values;
+	double *knots;
 	double **differ_quotient;
 
-	knots[0] = -5;
-	// knots[1] = -2;
-	// knots[2] = -0.5;
-	// knots[3] = 0;
-	// knots[4] = 0.5;
-	// knots[5] = 2;
-	knots[6] = 5;
 
-	double step = (abs (-5) + abs (5)) / static_cast<double>((N - 1));
-	for (int i = 1; i <= N - 2; i++) // dla wezlow rownoodleglych
-		knots[i] = -5 + (step * i);
+		knots = new double [N];
+		function_values = new double [N];
+
+		knots[0] = -5;
+//	 knots[1] = -2;
+//	 knots[2] = -0.5;
+//	 knots[3] = 0;
+//	 knots[4] = 0.5;
+//	 knots[5] = 2;
+		knots[N-1] = 5;
+
+		double step = (abs (-5) + abs (5)) / static_cast<double>((N - 1));
+		for (int i = 1; i <= N - 2; i++) // dla wezlow rownoodleglych
+		{
+			knots[i] = -5 + (step * i);
+//			std::cout << knots[i] << " ";
+		}
+//		std::cout << std::endl << std::endl;
 
 
-
-	differ_quotient = new double *[N];
-	fill_function_values (knots, function_values, N);
-
+<<<<<<< HEAD
 	for (int i = 0; i < N; i++)
 	{
 		differ_quotient[i] = new dou7mazur@stud205-14:~/Metody_numeryczne/Metody-Numeryczne-4th-sem-/Lab_07/Zestaw_2$ gnuplot gnuplot.sh 
 le[N + 2];
 	}
+=======
+		differ_quotient = new double *[N];
+		fill_function_values (knots, function_values, N);
+>>>>>>> a5f969dd22b7477de283766c34a11c783cc0ae90
 
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N + 2; j++)
-			differ_quotient[i][j] = 0;
-	}
+		for (int i = 0; i < N; i++)
+		{
+			differ_quotient[i] = new double[N + 2];
+		}
 
-	for (int i = 0; i < N; i++)
-	{
-		differ_quotient[i][0] = knots[i];
-		differ_quotient[i][1] = function_values[i];
-	}
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N + 2; j++)
+				differ_quotient[i][j] = 0;
+		}
 
-	difference_quotient (knots, function_values, &differ_quotient, N);
+		for (int i = 0; i < N; i++)
+		{
+			differ_quotient[i][0] = knots[i];
+			differ_quotient[i][1] = function_values[i];
+		}
 
-	FILE *fp;
-	fp = fopen ("dane2.dat", "w");
+		difference_quotient (knots, function_values, &differ_quotient, N);
 
-	if (!fp)
-		exit (-2);
+		FILE *fp;
+		fp = fopen ("dane.dat", "w");
 
+		if (!fp)
+			exit (-2);
 
-	for (double i = -5; i < 5.001; i += 0.01)
-	{
-		fprintf (fp, "%lf %lf %lf\n", i, 1.0 / (1.0 + pow (i, 2)), interpolate (knots, differ_quotient, N, i));
-	}
+		double temp = 0;
+		for (double i = -5; i < 5.001; i += 0.01)
+		{
+			fprintf (fp, "%lf %lf %lf\n", i, 1.0 / (1.0 + pow (i, 2)), interpolate (knots, differ_quotient, N, i));
 
+		}
 
 
 // free
@@ -70,6 +84,6 @@ le[N + 2];
 	delete[] differ_quotient;
 	delete[] knots;
 	delete[] function_values;
-	fclose (fp);
+//	fclose (fp);
 	return 0;
 }
--- Lab_07/Zestaw_2/gnuplot.sh
+++ Lab_07/Zestaw_2/gnuplot.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-set terminal post enhanced colour solid font 20  # wybor formatu, w jakim chcemy utworzyc wynikowy rysunek
-set output "cos2.eps" # nazwa pliku wynikowego
-set title "nwm" # tytul wykresu
-set xlabel "args" # etykieta osi OX
-set ylabel "values" # etykieta osi OY
+set terminal post enhanced colour solid font 14  # wybor formatu, w jakim chcemy utworzyc wynikowy rysunek
+set terminal jpeg
+set output "wykres1.jpg" # nazwa pliku wynikowego
+set title "Interpolacja Newtona dla rownoodleglych wezlow" # tytul wykresu
+set xlabel "argumenty funkcji" # etykieta osi OX
+set ylabel "wartosci funkcji" # etykieta osi OY
 set grid # wlaczenie widoczności siatki pomocniczej
 #set xrange [-11:11]
-
-plot 'Build/dane2.dat' using 1:2 with lines title "wykres funkcji", 'Build/dane2.dat' using 1:3 with points title "dopasowanie metoda interpolacji"
+plot 'cmake-build-debug/dane.dat' using 1:2 with lines title "wykres funkcji", 'cmake-build-debug/dane.dat' using 1:3 with points title "dopasowanie metoda interpolacji"
 #replot "cmake-build-debug/dane.dat" using 1:3 with points
 
 
