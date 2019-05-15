9e3545faf62660a280a62d74891417408a4a68d3
hubert-mazur <hiusvii@gmail.com>
2019-05-15 22:54:25 +0200

    Numerical analysis

---
+++ Lab_10/Zestaw_2/Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Main.h"
+
+
+int main ()
+{
+	woof (-0.5, 1.0, R1, "f(x)_1.dat", &function_1, -0.1673198);
+	woof (-0.5, 1.0, R2, "f(x)_2.dat", &function_1, -0.1673198);
+	woof (-4.0, 1.0, R1, "g(x)_1.dat", &function_2, 0.0);
+	woof (-4.0, 1.0, R2, "g(x)_2.dat", &function_2, 0.0);
+}
+
+
+double function_1 (double arg)
+{
+	return log (pow (arg, 5) + 3 * pow (arg, 2) + arg + 9);
+}
+
+double function_2 (double arg)
+{
+	return pow (arg, 6);
+}
+
+void
+woof (double left_init, double right_init, double R, std::string str, double (*function) (double), double exact_value)
+{
+	double x_a = left_init;
+	double x_b = right_init;
+	double x_1;
+	double x_2;
+	double x_min;
+	int IT = 0;
+
+	FILE *fp = fopen (&str[0], "w");
+
+	while (true)
+	{
+		x_1 = x_a + pow (R, 2) * (x_b - x_a);
+		x_2 = x_a + R * (x_b - x_a);
+
+		if (function (x_1) > function (x_2))
+			x_a = x_1;
+		else
+			x_b = x_2;
+
+		x_min = (x_1 + x_2) / 2.0;
+
+		IT++;
+
+		fprintf (fp, "%d %g %g\n", IT, x_min, fabs (x_min - exact_value));
+
+
+		if ((fabs (x_1 - x_2)) < epsilon)
+			break;
+	}
+	fclose (fp);
+
+}
---
+++ Lab_10/Zestaw_2/include/Main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <math.h>
+#include <string>
+
+const double R1 = (sqrt (5) - 1.0) / 2.0;
+const double R2 = 1.0 / 3.0;
+const double epsilon = pow (10, -6);
+
+double function_1 (double arg);
+
+double function_2 (double arg);
+
+void
+woof (double left_init, double right_init, double R, std::string str, double (*function) (double), double exact_value);
+
